fd7189315dab8a8e59cf4c5a4a62f6bac7e44245
mlavin <markdlavin@gmail.com>
2015-10-07 21:18:29 -0400

    Add project as an app.

--- project_name/settings.py
+++ project_name/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Application definition
 
 INSTALLED_APPS = (
+    '{{ project_name }}',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
